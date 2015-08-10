d30500da3c96ccc9538d033b258330657c15b56f
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-10 22:48:24 +0300

    updated config repository

--- ConfigRepository.php
+++ ConfigRepository.php
@@ -10,8 +10,45 @@
 
 namespace Anonym\Components\View;
 
-
+/**
+ * Class ConfigRepository
+ * @package Anonym\Components\View
+ */
 class ConfigRepository
 {
 
+    /**
+     *Dosyaların tutulacağı yolu tutar
+     *
+     *
+     * @var string
+     */
+    private $rootDir;
+
+    /**
+     *Dosyanın uzantı tipini tutar
+     *
+     * @var string
+     */
+    private $fileExtension;
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return $this->rootDir;
+    }
+
+    /**
+     * @param string $rootDir
+     * @return ConfigRepository
+     */
+    public function setRootDir($rootDir)
+    {
+        $this->rootDir = $rootDir;
+        return $this;
+    }
+
 }
+
