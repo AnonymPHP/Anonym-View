37d348539255041d06667204e6e0960c75720063
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-10 17:34:33 +0300

    some updates

--- ViewInterface.php
+++ ViewInterface.php
@@ -10,8 +10,28 @@
 
 namespace Anonym\Components\View;
 
-
+/**
+ * Interface ViewInterface
+ * @package Anonym\Components\View
+ */
 interface ViewInterface
 {
 
+    /**
+     * Yeni veri atamasý yapar
+     *
+     * @param string $name Veri ismi
+     * @param mixed $value Veriye atanacak deðer
+     * @return mixed
+     */
+    public function assign($name = '', $value);
+
+    /**
+     * Çýktýyý postolar
+     *
+     * @param string $name Çýktýlanacak gönderinin adý
+     * @param array $params Tasarým a yollanacak deðerler
+     * @return mixed
+     */
+    public function execute($name = '', array $params = []);
 }
