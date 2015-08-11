fedad871841af4b527719a55fe5901240d62a053
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-11 17:38:16 +0300

    some updates

---
+++ RepositoryManager.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
+
+
+namespace Anonym\Components\View;
+
+
+class RepositoryManager
+{
+    /**
+     *Parametreleri depolar
+     *
+     *
+     * @var ParameterRepository
+     */
+    private $paramsRepository;
+
+    /**
+     * Dosya isimlerini depolar
+     *
+     * @var FileNameRepository
+     */
+    private $nameRepository;
+
+    /**
+     *Ayarları tutacak objeyi depolar
+     *
+     * @var ConfigRepository
+     */
+    private $configRepository;
+
+    /**
+     *İçeriği depolayacak objeyi tutar
+     *
+     *
+     * @var ContentRepository
+     */
+    private $contentRepository;
+
+
+    /**
+     * @return ConfigRepository
+     */
+    public function getConfigRepository()
+    {
+        return $this->configRepository;
+    }
+
+    /**
+     * @param ConfigRepository $configRepository
+     * @return View
+     */
+    public function setConfigRepository(ConfigRepository $configRepository)
+    {
+        $this->configRepository = $configRepository;
+        return $this;
+    }
+
+
+    /**
+     * @return ParameterRepository
+     */
+    public function getParamsRepository()
+    {
+        return $this->paramsRepository;
+    }
+
+    /**
+     * @param ParameterRepository $paramsRepository
+     * @return View
+     */
+    public function setParamsRepository(ParameterRepository $paramsRepository)
+    {
+        $this->paramsRepository = $paramsRepository;
+        return $this;
+    }
+
+    /**
+     * @return FileNameRepository
+     */
+    public function getNameRepository()
+    {
+        return $this->nameRepository;
+    }
+
+    /**
+     * @param FileNameRepository $nameRepository
+     * @return View
+     */
+    public function setNameRepository(FileNameRepository $nameRepository)
+    {
+        $this->nameRepository = $nameRepository;
+        return $this;
+    }
+
+    /**
+     * @return ContentRepository
+     */
+    public function getContentRepository()
+    {
+        return $this->contentRepository;
+    }
+
+    /**
+     * @param ContentRepository $contentRepository
+     * @return View
+     */
+    public function setContentRepository(ContentRepository $contentRepository)
+    {
+        $this->contentRepository = $contentRepository;
+        return $this;
+    }
+
+    /**
+     *  Ön tanımlı değerleri atar
+     */
+    protected function setDefaultVars()
+    {
+        $this->setConfigRepository( new ConfigRepository());
+        $this->setNameRepository( new FileNameRepository());
+        $this->setParamsRepository( new ParameterRepository());
+    }
+
+}
+
--- View.php
+++ View.php
@@ -14,40 +14,11 @@ namespace Anonym\Components\View;
  * Class View
  * @package Anonym\Components\View
  */
-class View
+class View extends RepositoryManager
 {
 
     use FilePathGenerator;
 
-    /**
-     *Parametreleri depolar
-     *
-     *
-     * @var ParameterRepository
-     */
-    private $paramsRepository;
-
-    /**
-     * Dosya isimlerini depolar
-     *
-     * @var FileNameRepository
-     */
-    private $nameRepository;
-
-    /**
-     *Ayarları tutacak objeyi depolar
-     *
-     * @var ConfigRepository
-     */
-    private $configRepository;
-
-    /**
-     *İçeriği depolayacak objeyi tutar
-     *
-     *
-     * @var ContentRepository
-     */
-    private $contentRepository;
 
     /**
      * Sınıfı başlatır ve atamaları yapar
@@ -55,85 +26,8 @@ class View
      */
     public function __construct()
     {
-        $this->setConfigRepository( new ConfigRepository());
-        $this->setNameRepository( new FileNameRepository());
-        $this->setParamsRepository( new ParameterRepository());
+        $this->setDefaultVars();
     }
-
-    /**
-     * @return ConfigRepository
-     */
-    public function getConfigRepository()
-    {
-        return $this->configRepository;
-    }
-
-    /**
-     * @param ConfigRepository $configRepository
-     * @return View
-     */
-    public function setConfigRepository(ConfigRepository $configRepository)
-    {
-        $this->configRepository = $configRepository;
-        return $this;
-    }
-
-
-    /**
-     * @return ParameterRepository
-     */
-    public function getParamsRepository()
-    {
-        return $this->paramsRepository;
-    }
-
-    /**
-     * @param ParameterRepository $paramsRepository
-     * @return View
-     */
-    public function setParamsRepository(ParameterRepository $paramsRepository)
-    {
-        $this->paramsRepository = $paramsRepository;
-        return $this;
-    }
-
-    /**
-     * @return FileNameRepository
-     */
-    public function getNameRepository()
-    {
-        return $this->nameRepository;
-    }
-
-    /**
-     * @param FileNameRepository $nameRepository
-     * @return View
-     */
-    public function setNameRepository(FileNameRepository $nameRepository)
-    {
-        $this->nameRepository = $nameRepository;
-        return $this;
-    }
-
-    /**
-     * @return ContentRepository
-     */
-    public function getContentRepository()
-    {
-        return $this->contentRepository;
-    }
-
-    /**
-     * @param ContentRepository $contentRepository
-     * @return View
-     */
-    public function setContentRepository(ContentRepository $contentRepository)
-    {
-        $this->contentRepository = $contentRepository;
-        return $this;
-    }
-
-
 
 
 }
