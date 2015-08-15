432a8b801475e5d500270b7878cd5383a3f8f497
anonymframework <serif.saglam119@outlook.com>
2015-08-15 17:39:07 +0300

    made some updates on language manager

--- LanguageManager.php
+++ LanguageManager.php
@@ -11,8 +11,31 @@
 
 namespace Anonym\Components\View;
 
-
+/**
+ * the class of language manager
+ *
+ * Class LanguageManager
+ * @package Anonym\Components\View
+ */
 class LanguageManager
 {
+
+    /**
+     * the path of language files
+     *
+     * @var string
+     */
+    private $path;
+
+    /**
+     * get the language variables with file path
+     *
+     * @param string $file the file name
+     * @return array registered varibles on file
+     */
+    public function getLanguage($file = '')
+    {
+
+    }
 
 }
