ca91d3a8d6d62e0c068076ccabb854168398da53
anonymframework <serif.saglam119@outlook.com>
2015-09-22 23:10:37 +0300

    made some updates

--- View.php
+++ View.php
@@ -10,8 +10,25 @@
 
 namespace Anonym\Components\View;
 
-
-class View
+/**
+ * Class View
+ * @package Anonym\Components\View
+ */
+class View extends Blade
 {
+
+    /**
+     * create a new instance and register blade
+     *
+     * @param array $configs
+     */
+    public function __construct(array $configs = []){
+        $cache = isset($configs['cache']) ? $configs['cache'] : RESOURCE.'cache/';
+        $view =  isset($configs['view']) ? $configs['view'] : VIEW;
+
+
+        parent::__construct($view, $cache);
+     }
+
 
 }
