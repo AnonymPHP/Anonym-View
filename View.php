44939da2dc8ddf99dd3a970fe0f15b4ab5c634d4
anonymframework <serif.saglam119@outlook.com>
2015-09-23 01:16:43 +0300

    made some updates

--- View.php
+++ View.php
@@ -18,6 +18,12 @@ class View extends Blade
 {
 
     /**
+     * the instance of language manager
+     *
+     * @var LanguageManager
+     */
+    protected  $languageManager;
+    /**
      * create a new instance and register blade
      *
      * @param array $configs
@@ -26,10 +32,22 @@ class View extends Blade
     {
         $cache = isset($configs['cache']) ? $configs['cache'] : RESOURCE . 'cache/';
         $view = isset($configs['view']) ? $configs['view'] : VIEW;
+        $language = isset($configs['language']) ? $configs['language'] : LANGUAGE;
+
+        $this->languageManager = new LanguageManager($language);
 
         parent::__construct($view, $cache);
     }
 
+    /**
+     * get the language parameters
+     *
+     * @param string $lang
+     * @return array|bool
+     */
+    public function lang($lang = ''){
+        return $this->languageManager->getLanguage($lang);
+    }
 
     /**
      * if called method not found in this class, call it in view.
