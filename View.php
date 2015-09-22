b97e725c3b519b6517bd03b1d5470ca18b39132c
anonymframework <serif.saglam119@outlook.com>
2015-09-22 23:30:51 +0300

    made some updates

--- View.php
+++ View.php
@@ -22,13 +22,22 @@ class View extends Blade
      *
      * @param array $configs
      */
-    public function __construct(array $configs = []){
-        $cache = isset($configs['cache']) ? $configs['cache'] : RESOURCE.'cache/';
-        $view =  isset($configs['view']) ? $configs['view'] : VIEW;
+    public function __construct(array $configs = [])
+    {
+        $cache = isset($configs['cache']) ? $configs['cache'] : RESOURCE . 'cache/';
+        $view = isset($configs['view']) ? $configs['view'] : VIEW;
 
 
         parent::__construct($view, $cache);
-     }
+    }
 
+    /**
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
+    public function __call($method, array $args){
+        return call_user_func_array([$this->view(), $method], $args);
+    }
 
 }
